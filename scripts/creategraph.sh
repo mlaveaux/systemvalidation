f102be02f5b34490aeec1dc6adf29571fa9917ca
Maurice Laveaux <mauricelaveaux@gmail.com>
2015-09-19 15:58:12 +0200

    Added a simple script and initial component

---
+++ scripts/creategraph.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+MCRL2_FILENAME=$1.mcrl2 # The mcrl2 readable file.
+LPS_FILENAME=$1.lps # A linear process specification file.
+LTS_FILENAME=$1.lts # A labelled transition system file..
+
+if [ ! -e "${MCRL2_FILENAME}" ]; then
+    echo "Requires a input <file>.mcrl2 argument"; exit;
+fi
+
+mcrl22lps ${MCRL2_FILENAME} ${LPS_FILENAME}
+lps2lts ${LPS_FILENAME} ${LTS_FILENAME}
+ltsgraph ${LTS_FILENAME}
