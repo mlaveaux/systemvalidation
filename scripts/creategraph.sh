4d6dedb0e65e3d763374ed48b60906e05c0b53f2
Maurice Laveaux <mauricelaveaux@gmail.com>
2015-10-07 14:24:04 +0200

    Fixed something in script and update gitignore.

--- scripts/creategraph.sh
+++ scripts/creategraph.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-MCRL2_FILENAME=$1.mcrl2 # The mcrl2 readable file.
-LPS_FILENAME=$1.lps # A linear process specification file.
-LTS_REDUCED_FILENAME=$1.reduced.lps # A linear process specification file.
-LTS_FILENAME=$1.lts # A labelled transition system file..
+MCRL2_FILENAME="$1.mcrl2" # The mcrl2 readable file.
+LPS_FILENAME="$1.lps" # A linear process specification file.
+LTS_REDUCED_FILENAME="$1.reduced.lps" # A linear process specification file.
+LTS_FILENAME="$1.lts" # A labelled transition system file..
 
 if [ ! -e "${MCRL2_FILENAME}" ]; then
     echo "Requires a input <file>.mcrl2 argument"; exit;
