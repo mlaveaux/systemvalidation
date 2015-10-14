f53d25965822534e6365944ab44c6fa23ece0e0c
Maurice Laveaux <mauricelaveaux@gmail.com>
2015-10-14 14:13:11 +0200

    Updated some scripts.

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. ./$(dirname $0)/makemodel.sh $1 &&
+sh ./$(dirname $0)/makemodel.sh $1 &&
 
 LTS_FILENAME="$1.lts"
 STATEFORMULA_FILENAME="$2.mcf"
@@ -11,5 +11,7 @@ if [ ! -e ${LTS_FILENAME} ] || [ ! -e ${STATEFORMULA_FILENAME} ]; then
     exit -1;
 fi
 
+echo ${STATEFORMULA_FILENAME}:
+
 lts2pbes --formula=${STATEFORMULA_FILENAME} ${LTS_FILENAME} ${PBES_FILENAME} &&
-pbes2bool --counter ${PBES_FILENAME}
+pbes2bool ${PBES_FILENAME}
---
+++ scripts/verifyall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+if [ -z "$1" ] || [  -z "$2" ]; then
+    echo "Input format is <filename>.lts <dirname>"; exit -1;
+fi
+
+FILES=$(dir $2)
+SUFFIX=".mcf"
+
+for file in $FILES; do
+    case "$file" in
+        *".mcf"*)
+            FILE=$file
+            FILE=${FILE%$SUFFIX}
+            sh ./$(dirname $0)/verify.sh $1 $2/$FILE
+        ;;
+    esac
+done
